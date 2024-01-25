c5b23466f0b4477a2983f269dd0a566be7f701b3
Aralta <62393728+Aralta@users.noreply.github.com>
2024-01-25 21:17:54 +0400

    Update data.py

--- data.py
+++ data.py
@@ -3,37 +3,14 @@ from pymongo import MongoClient
 import os
 
 client = MongoClient("mongodb://vps1.amoros.io:32777")
-db = client["database2"]
+db = client["database"]
 
 AA = ["A", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "Y"]
 
 
 
-# Collection pour les données du fichier TSV
-tsv_collection_name = "taxo"
-tsv_collection = db[tsv_collection_name]
-
-# Chemin du fichier TSV
-tsv_file_path = "data/taxo_filtered.tsv"
-
-# Charger le fichier TSV en utilisant pandas
-df = pd.read_csv(tsv_file_path, sep='\t')
-
-# Convertir le DataFrame en dictionnaires (une ligne par document)
-data_dict = df.to_dict(orient='records')
-
-# Insérer les données dans la nouvelle collection MongoDB
-tsv_collection.insert_many(data_dict)
-
-print("Importation des données TSV dans MongoDB terminée.")
-
-
-
-
-
-# Nom de la collection MongoDB
-collection_name = "freq_taxon"
-collection = db[collection_name]
+# collection MongoDB
+collection = db["freq_taxon"]
 x=0
 for i in range(len(AA)):
     for j in range(len(AA)):
